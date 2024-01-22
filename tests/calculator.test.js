790a814e15ae510d21d57df007967bfd2681354e
PaolaLoera <perl41997@gmail.com>
2024-01-22 13:37:07 -0600

    Calc y Users

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,25 @@
-const { default: expect } = require('expect');
+// const { default: expect } = require('expect');
 
+// const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
-test('adds a + b', () => {
-    //
+test('adds a + b to equal 3', () => {
+    expect(calculator.sum(1, 2)).toBe(3);
 })
 
-test('substracts a - b', () => {
-   //
+test('substracts a - b to equal 1', () => {
+   expect(calculator.substract(2,1)).toBe(1);
 })
 
-test('multiplies a * b', () => {
-    //
+test('multiplies a * b to equal 4', () => {
+    expect(calculator.multiply(2,2)).toBe(4);
 })
 
-test('divides a / b', () => {
-    // Consider decimals
-})
+test('divides a / b to equal 3', () => {
+    expect(calculator.divide(6, 2)).toBeCloseTo(3);
+});
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(2,2)).toBe(4);
 })
 
--- users.js
+++ users.js
@@ -12,8 +12,34 @@ const getUser = id => {
     return users.find(user => user.id === id)
 }
 
+// By password
+const getPass= password => {
+    return users.find(users => users.password === password)
+}
 
 // test
+describe('User functions test', () => {
+    it('Should get the user by its id', () => {
+        const user = getUser(3);
+        expect(user).toEqual ({
+            id: 3,
+            username: "goosemotionless",
+            email: "dotmeheart@outlook.com",
+            password: "12345abcde"
+        })
+    })
+})
 // console.log(getUser(3));
+describe('User functions test', () => {
+    it('Should get the user by its password', () => {
+        const user = getPass("hArrydotCom");
+        expect(user).toEqual ({
+            id: 1,
+            username: "hpLover4",
+            email: "sirious90@gmail.com",
+            password: "hArrydotCom"
+        })
+    })
+})
 
 module.exports = {getUsers , getUser};
